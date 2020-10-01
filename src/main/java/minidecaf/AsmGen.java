5c26426bc7777ed9b24e07b89b2ec52f4ff08fe8
Xiangze-Li <lee_johnson@qq.com>
2020-10-01 19:51:06 +0800

    compiler step2 /wo readme

--- src/main/java/minidecaf/AsmGen.java
+++ src/main/java/minidecaf/AsmGen.java
@@ -5,33 +5,38 @@ import java.util.Scanner;
 /**
  * AsmGen
  */
-public class AsmGen
-{
-    private AsmGen()
-    {}
+public class AsmGen {
+    private AsmGen() {
+    }
 
-    public static String genAsm(final String ir)
-    {
+    public static String genAsm(final String ir) {
         StringBuilder sb = new StringBuilder();
 
-        try (Scanner irIn = new Scanner(ir))
-        {
-            while (irIn.hasNextLine())
-            {
+        try (Scanner irIn = new Scanner(ir)) {
+            while (irIn.hasNextLine()) {
                 String[] sp = irIn.nextLine().strip().split(" ");
 
-                switch (sp[0])
-                {
-                    case "func" :
+                switch (sp[0]) {
+                    case "func":
                         doFunc(sb, sp[1]);
                         break;
-                    case "push" :
+                    case "push":
                         doPush(sb, sp[1]);
                         break;
-                    case "ret" :
+                    case "ret":
                         doRet(sb);
                         break;
-                    default :
+                    case "notl":
+                        doUnaryOp(sb, "seqz");
+                        break;
+                    case "notb":
+                        doUnaryOp(sb, "not");
+                        break;
+                    case "neg":
+                        doUnaryOp(sb, "neg");
+                        break;
+                    default:
+                        assert (false);
                         break;
                 }
             }
@@ -41,22 +46,36 @@ public class AsmGen
     }
 
     private static void doFunc(final StringBuilder sb, final String label) {
-        sb.append("\t.text\n\t.global " + label.substring(0, label.length() - 1) + "\n");
+        sb.append("\t.text\n\t.global ").append(label).append("\n");
         doLabel(sb, label);
     }
 
-    private static void doLabel(final StringBuilder sb, final String label)
-    {
-        sb.append(label + "\n");
+    private static void doLabel(final StringBuilder sb, final String label) {
+        sb.append(label).append(":\n");
     }
 
-    private static void doPush(final StringBuilder sb, final String pushWhat)
-    {
-        sb.append("\taddi sp,sp,-4\n").append("\tli t1," + pushWhat + "\n").append("\tsw t1,0(sp)\n");
+    private static void doPush(final StringBuilder sb, final String pushWhat) {
+        sb.append("\tli t1,").append(pushWhat).append("\n");
+        mPush(sb, "t1");
     }
 
-    private static void doRet(final StringBuilder sb)
-    {
-        sb.append("\tlw a0,0(sp)\n\taddi sp,sp,4\n\tjr ra\n");
+    private static void doRet(final StringBuilder sb) {
+        mPop(sb, "a0");
+        sb.append("\tjr ra\n");
     }
+
+    private static void doUnaryOp(final StringBuilder sb, final String whatOp) {
+        mPop(sb, "t1");
+        sb.append("\t").append(whatOp).append(" t1,t1\n");
+        mPush(sb, "t1");
+    }
+
+    private static void mPop(final StringBuilder sb, final String toWhere) {
+        sb.append("\tlw ").append(toWhere).append(",0(sp)\n").append("\taddi sp,sp,4\n");
+    }
+
+    private static void mPush(final StringBuilder sb, final String fromWhere) {
+        sb.append("\taddi sp,sp,-4\n").append("sw ").append(fromWhere).append(",0(sp)\n");
+    }
+
 }
