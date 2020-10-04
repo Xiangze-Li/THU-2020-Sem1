0698ebf7ea080cec89842d7ccc67e47de5179ddb
Xiangze-Li <lee_johnson@qq.com>
2020-10-04 09:14:00 +0800

    init se hw-2

---
+++ codes/se/MessageBoard2020/src/views/Stage3.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="stage3">
+    <!--修改下方的MessageList和PostDialog-->
+    <MessageList/>
+    <PostDialog/>
+  </div>
+</template>
+
+<script>
+import MessageList from "@/components/MessageList.vue"
+import PostDialog from "@/components/PostDialog.vue"
+
+export default {
+  name: 'Stage3',
+  components: {
+    MessageList,
+    PostDialog
+  },
+  // 请在下方设计自己的数据结构和响应函数
+  data(){
+    return {
+      messageList: [],
+      dialogVisible: true,
+    }
+  },
+  methods: {
+  }
+}
+</script>
