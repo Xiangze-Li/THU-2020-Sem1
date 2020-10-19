12e1feeac2802548d2c0b6f9e6f5f289c2544f34
Xiangze-Li <lee_johnson@qq.com>
2020-10-19 11:04:45 +0800

    network exp-checksum done

--- codes/router/Homework/checksum/checksum.cpp
+++ codes/router/Homework/checksum/checksum.cpp
@@ -7,7 +7,18 @@
  * @param len 即 packet 的长度，单位是字节，保证包含完整的 IP 头
  * @return 校验和无误则返回 true ，有误则返回 false
  */
-bool validateIPChecksum(uint8_t *packet, size_t len) {
-  // TODO:
-  return true;
+bool validateIPChecksum(uint8_t *packet, size_t len)
+{
+    // TODO:
+    int headLen = (packet[0] & 0x0F) * 4;
+    int sum = 0;
+    for (size_t i = 0; i < headLen; i++)
+    {
+        sum += packet[i];
+        while (sum & ~0xFF)
+        {
+            sum = (sum >> 8) + (sum & 0xFF);
+        }
+    }
+    return !(~(sum | 0xFFFFFF00));
 }
