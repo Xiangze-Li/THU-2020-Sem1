eabad283db477363d4904add2893aba5d135d0d8
Xiangze-Li <lee_johnson@qq.com>
2020-11-02 19:27:26 +0800

    router lab-3 done

--- codes/router/Homework/lookup/lookup.cpp
+++ codes/router/Homework/lookup/lookup.cpp
@@ -1,6 +1,11 @@
 #include "router.h"
 #include <stdint.h>
 #include <stdlib.h>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+std::vector<RoutingTableEntry> routeTable;
 
 /**
  * @brief 插入/删除一条路由表表项
@@ -10,8 +15,38 @@
  * 插入时如果已经存在一条 addr 和 len 都相同的表项，则替换掉原有的。
  * 删除时按照 addr 和 len **精确** 匹配。
  */
-void update(bool insert, RoutingTableEntry entry) {
-  // TODO:
+void update(bool insert, RoutingTableEntry entry)
+{
+    // TODO:
+    if (insert)
+    {
+        bool found = false;
+        for (RoutingTableEntry &e : routeTable)
+        {
+            if (e.addr == entry.addr && e.len == entry.len)
+            {
+                e = entry;
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            routeTable.push_back(entry);
+        }
+    }
+    else // !insert == delete
+    {
+        for (size_t i = 0; i < routeTable.size(); i++)
+        {
+            RoutingTableEntry const &e = routeTable[i];
+            if (e.addr == entry.addr && e.len == entry.len)
+            {
+                routeTable.erase(routeTable.begin() + i);
+                break;
+            }
+        }
+    }
 }
 
 /**
@@ -21,9 +56,44 @@ void update(bool insert, RoutingTableEntry entry) {
  * @param if_index 如果查询到目标，把表项的 if_index 写入
  * @return 查到则返回 true ，没查到则返回 false
  */
-bool prefix_query(uint32_t addr, uint32_t *nexthop, uint32_t *if_index) {
-  // TODO:
-  *nexthop = 0;
-  *if_index = 0;
-  return false;
+bool prefix_query(uint32_t addr, uint32_t *nexthop, uint32_t *if_index)
+{
+    // TODO:
+    // fprintf(stderr, ">>>>\tQuerying 0x%08x\n", addr);
+
+    *nexthop = 0;
+    *if_index = 0;
+
+    const RoutingTableEntry *foundEntry = nullptr;
+
+    for (const RoutingTableEntry &e : routeTable)
+    {
+        if (e.len==0 && (!foundEntry || foundEntry->len <= 0)){
+            foundEntry = &e;
+            continue;
+        }
+
+        uint32_t mask = ((uint32_t)-1 >> (32 - e.len));
+
+        // fprintf(stderr, "    \tRoute table entry 0x%08x %d; mask 0x%08x\n", e.addr, e.len, mask);
+        // fprintf(stderr, "    \te.addr ^ addr == %08x\n", e.addr ^ addr);
+        if (!(mask & (e.addr ^ addr)))
+        {
+            // fprintf(stderr, "    \tHit.\n");
+            if (!foundEntry || foundEntry->len <= e.len)
+            {
+                foundEntry = &e;
+                // fprintf(stderr, "    \tUpdate.\n");
+            }
+        }
+    }
+
+    if (!foundEntry)
+        return false;
+    else
+    {
+        *nexthop = foundEntry->nexthop;
+        *if_index = foundEntry->if_index;
+        return true;
+    }
 }
